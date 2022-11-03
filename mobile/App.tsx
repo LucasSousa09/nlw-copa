757cd3727d9e54d96e1cc9730ab19be7dfa3f96e
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-11-03 12:45:41 -0300

    mobile layout and structure completed

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,15 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { THEME } from './src/styles/theme'
+import { THEME } from './src/styles/theme';
 
-import { SignIn } from './src/components/SignIn';
-import { Loading } from './src/components/Loading'
+import { New } from './src/screens/New'
+import { Find } from './src/screens/Find'
+import { Pools } from './src/screens/Pools'
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
 
@@ -16,12 +21,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={ THEME }>
+      <AuthContextProvider>
         <StatusBar 
           barStyle='light-content'
           backgroundColor='transparent'
           translucent
-        />
-        { fontsLoaded ? <SignIn /> : <Loading /> }
+          />
+        { fontsLoaded ? <Pools /> : <Loading /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/components/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Text } from 'native-base'
-
-export function SignIn(){
-  return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={32} fontWeight="bold">SignIn</Text> 
-    </Center>
-  )
-}
